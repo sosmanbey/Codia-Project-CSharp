36aa5dacd526e4bb2b82e3d47526b9912b9155a9
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-25 11:58:46 +0300

    Add files via upload

---
+++ Faktöriyel Hesaplama V1.0/Faktöriyel Hesaplama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Faktöriyel_Hesaplama
+{
+    class Program
+    {
+        static double faktoriyel(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * faktoriyel(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0)
+            {
+                double sonuc = faktoriyel(sayi);
+                Console.WriteLine("{0}!={1}", sayi, sonuc);
+            }
+            Console.ReadKey();
+        }
+    }
+}
