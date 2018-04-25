f4b64e97dababf3603b5c1ff99ee996a890d4fa2
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-26 01:01:21 +0300

    Add files via upload

---
+++ Yazılan Sayının Rakamlarının Toplanması/Yazılan Sayının Rakamlarının Toplanması/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Yazılan_Sayının_Rakamlarının_Toplanması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, sonuc, rakam;
+            int sıfır = 0;
+            Console.Write("Sayıyı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            sonuc = sayi.ToString().Length;
+            for (int i = 0; i < sonuc; i++)
+            {
+                rakam = int.Parse(sayi.ToString().Substring(i, 1));
+                sıfır = rakam + sıfır;
+            }
+            Console.WriteLine("Girilen sayının rakamları toplamı= " + sıfır);
+
+            Console.ReadKey();
+        }
+    }
+}
