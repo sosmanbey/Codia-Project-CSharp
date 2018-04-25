0c4134404b12a9da972fb692d93699d372926fca
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-25 12:22:31 +0300

    Add files via upload

---
+++ Faktöriyel Hesaplama v1.1/Faktöriyel/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faktöriyel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 1;
+        int faktöriyel = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (i = 1; i <= 5; i++)
+            {
+                faktöriyel = faktöriyel * i;
+                label1.Text = faktöriyel.ToString();
+            }
+        }
+    }
+}
