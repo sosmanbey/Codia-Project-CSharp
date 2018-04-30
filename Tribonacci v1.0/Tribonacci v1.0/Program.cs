cd735f537b10d11666e9f9915917726a3b6aa173
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 04:27:00 +0300

    Add files via upload

---
+++ Tribonacci v1.0/Tribonacci v1.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tribonacci_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 2;
+            int d;
+            Console.WriteLine(a); //bu üç satırı yazmasaydık direk
+            Console.WriteLine(b); //4,7,13,24 olarak devam edecekti.
+            Console.WriteLine(c); //10 elemandan oluşmuş olması için bu üç satırı da yazdık.
+            for (int i = 1; i <= 7; i++)
+            {
+                d = a + b + c;
+                a = b;
+                b = c;
+                c = d;
+                Console.WriteLine(d);
+            }
+            Console.ReadKey();
+        }
+    }
+}
