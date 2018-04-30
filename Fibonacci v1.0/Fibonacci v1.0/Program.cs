6d058d46256e3ea6c4bb6d6e1b12120f9ab86d0b
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 04:25:59 +0300

    Add files via upload

---
+++ Fibonacci v1.0/Fibonacci v1.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c;
+            Console.WriteLine(a); //bu iki satırı yazmasaydık direk
+            Console.WriteLine(b); //2,3,5,8 olarak devam edecekti.10 elemandan oluşmuş olması için.
+            for (int i = 1; i <= 8; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
