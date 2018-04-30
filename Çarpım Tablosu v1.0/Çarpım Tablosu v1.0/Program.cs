a2d746efd0edf2e812970bb9585507dd0320547f
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 04:24:50 +0300

    Add files via upload

---
+++ Çarpım Tablosu v1.0/Çarpım Tablosu v1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Çarpım_Tablosu_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("İlk sayıyı giriniz: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a * b;
+            Console.WriteLine("Çarpım: " + c);
+            Console.ReadKey();
+        }
+        
+    }
+}
