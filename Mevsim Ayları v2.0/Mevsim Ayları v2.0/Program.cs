3439fb23f61c6926fbff834c3063992f934f395f
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 14:01:45 +0300

    Add files via upload

---
+++ Mevsim Ayları v2.0/Mevsim Ayları v2.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mevsim_Ayları_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mevsim;
+            Console.Write("Lütfen mevsimi küçük harflerle giriniz: ");
+            mevsim = Console.ReadLine();
+
+            switch (mevsim)
+            {
+                case "yaz": Console.Write("Yaz mevsimi ayları: Haziran-Temmuz-Ağustos");
+                    break;
+                case "sonbahar": Console.Write("Sonbahar mevsimi ayları: Eylül-Ekim-Kasım");
+                   break;
+                case "kış": Console.Write("Kış mevsimi ayları: Aralık-Ocak-Şubat");
+                    break;
+                case "ilkbahar":Console.Write("İlkbahar mevsimi ayları: Mart-Nisan-Mayıs");
+                    break;
+                default:Console.Write("Hatalı mevsim girişi.");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
