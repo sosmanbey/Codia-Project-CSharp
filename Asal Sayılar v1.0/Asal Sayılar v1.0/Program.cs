037d41d14401e655c29dcd586a3b70387cce9a3a
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 04:23:21 +0300

    Add files via upload

---
+++ Asal Sayılar v1.0/Asal Sayılar v1.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Asal_Sayılar_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //30'a kadar olan asal sayılar
+            int s;
+            for (int i = 2; i < 30; i++)
+            {
+                s = 0;
+                for (int j = 1; j < 30; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        s += 1;
+                    }
+                }
+                if (s == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
