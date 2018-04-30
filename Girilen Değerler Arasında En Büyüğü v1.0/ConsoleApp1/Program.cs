5048b205c6d9d5e9715115e4d98b7bd99434516f
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 13:59:58 +0300

    Add files via upload

---
+++ Girilen Değerler Arasında En Büyüğü v1.0/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayılar = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write((i + 1).ToString() + ". Sayıyı giriniz: ");
+                sayılar[i] =Convert.ToInt32(Console.ReadLine());
+            }
+            int enbuyuk;
+            enbuyuk = sayılar[0];
+            for (int i = 1; i < 5; i++) //0. index kendisi olduğu için i=1 den başladık.
+            {
+                if (enbuyuk < sayılar[i])
+                {
+                    enbuyuk = sayılar[i];
+                }
+            }
+            Console.Write(enbuyuk);
+            Console.Read();
+        }
+    }
+}
