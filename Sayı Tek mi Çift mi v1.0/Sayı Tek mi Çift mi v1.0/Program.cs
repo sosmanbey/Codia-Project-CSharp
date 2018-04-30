663e939965aaf8776c9ccbebc83ffa58f5e53607
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-04-30 14:02:45 +0300

    Add files via upload

---
+++ Sayı Tek mi Çift mi v1.0/Sayı Tek mi Çift mi v1.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sayı_Tek_mi_Çift_mi_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            // (sayi % 5 == 0) 5e tam bölünmeyi bulma.
+            if (sayi % 2 == 0)
+            {
+                Console.Write("Sayı çifttir.");
+            }
+            else
+            {
+                Console.Write("Sayı tektir.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
