8f4d055c6e2c562b6ea332895b51117215b4f28e
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-05-01 18:50:56 +0300

    Add files via upload

---
+++ Araba Park Süresi v1.0/Araba Park Süresi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Araba_Park_Süresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int süre;
+            Console.Write("Süreyi giriniz: ");
+            süre = Convert.ToInt32(Console.ReadLine());
+            if (süre > 0 && süre <= 2)
+            {
+                Console.WriteLine("Ücret 5TL");
+            }
+            else if (süre > 2 && süre <= 5)
+            {
+                Console.WriteLine("Ücret 10TL");
+            }
+            else if (süre > 5 && süre <= 12)
+            {
+                Console.WriteLine("Ücret 20TL");
+            }
+            else if (süre > 12 && süre <= 24)
+            {
+                Console.WriteLine("Ücret 30TL");
+            }
+            else
+            {
+                Console.WriteLine("Senelik abonemizin aylık ücreti 750TL");
+            }
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
