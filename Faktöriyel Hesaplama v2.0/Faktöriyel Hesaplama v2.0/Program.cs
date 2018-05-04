641a2b7c28d116b88dce0fe95da758f493a9c2b9
Şaban Osmanbey <37086705+sosmanbey@users.noreply.github.com>
2018-05-04 05:16:01 +0300

    Add files via upload

---
+++ Faktöriyel Hesaplama v2.0/Faktöriyel Hesaplama v2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Faktöriyel_Hesaplama_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int factoriel = 1;
+            Console.Write("Faktöriyeli alınacak sayıyı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            int[] sayilar = { sayi };
+            foreach (int i in sayilar)
+            {
+                for (int j = sayi; j > 0; j--)
+                {
+                    factoriel *= j;
+                }
+            }
+            Console.WriteLine("Faktöriyel: " + factoriel);
+
+
+
+            Console.Read();
+        }
+    }
+}
